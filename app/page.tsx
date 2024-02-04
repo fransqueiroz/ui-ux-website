3c6b54cea6a11c2bcb7bd7ee5cb8faa690a575c1
fransergio.queiroz <fransergiosq@gmail.com>
2024-02-04 12:29:07 -0300

    fist commit

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import { Faq } from "./components/Faq";
+import { Features } from "./components/Features";
+import { Hero } from "./components/Hero";
+import { Navbar } from "./components/Navbar";
+import { Pricing } from "./components/Pricing";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <div className="px-[20px] lg:container lg:px-20 mx-auto">
+        <Features/>
+        <Faq/>
+        <Pricing/>
+      </div>
+    </>
+  );
+}
